3a16be308806ae195c49bba2c7adc3fba4c43982
nolan887 <nolan887@gmail.com>
2020-12-31 07:57:18 -0500

    Etch-A-Sketch App challenge complete (Video 176)

--- Day19/day-19-start/main.py
+++ Day19/day-19-start/main.py
@@ -1,3 +1,35 @@
 # Day 19 Starting file
-# TODO: Complete Day 19 of 100 Days of Code
 
+
+from turtle import Turtle, Screen
+
+tim= Turtle()
+
+screen = Screen()
+screen.listen()
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.back(10)
+
+def turn_cw():
+    tim.right(10)
+
+def turn_ccw():
+    tim.left(10)
+
+def shake():
+    tim.penup()
+    tim.home()
+    tim.clear()
+    tim.pendown()
+
+screen.onkey(key = "w", fun = move_forwards)
+screen.onkey(key = "s", fun = move_backwards)
+screen.onkey(key = "d", fun = turn_cw)
+screen.onkey(key = "a", fun = turn_ccw)
+screen.onkey(key = "c", fun = shake)
+
+screen.exitonclick()
