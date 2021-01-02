378aa638d3bcd2a8cf4e8f298b814e09596979f8
nolan887 <nolan887@gmail.com>
2021-01-02 13:22:34 -0500

    Day 21 Progress - snake game complete (Video 192)

--- Day21/snake-part-2/main.py
+++ Day21/snake-part-2/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 # Screen Window Setup
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 game_on = True
 
@@ -27,9 +29,21 @@ while game_on:
     time.sleep(0.1)
     snake.move()
 
-#     Detect food collision
+    # Detect food collision
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
+        scoreboard.update_score()
 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
