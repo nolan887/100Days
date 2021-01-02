72963ab6d21c8766285be3c31184a629e1ca53a6
nolan887 <nolan887@gmail.com>
2021-01-01 20:33:43 -0500

    Day 20 progress (Video 182)

--- Day20/snake-part-1/main.py
+++ Day20/snake-part-1/main.py
@@ -1,1 +1,23 @@
-print("testing main is new default")
+from turtle import Turtle, Screen
+
+# Screen Window Setup
+screen = Screen()
+screen.setup(width = 600, height = 600)
+screen.bgcolor("black")
+screen.title("Snake")
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
+    new_segmenet = Turtle("square")
+    new_segmenet.color("yellow")
+    new_segmenet.goto(position)
+
+
+
+
+
+
+
+
+screen.exitonclick()
