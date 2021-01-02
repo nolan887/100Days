3e07146b614c7ef92aae5c908de1eb46285d92c9
nolan887 <nolan887@gmail.com>
2021-01-01 21:49:26 -0500

    Day 20 Complete (Video 186)

--- Day20/snake-part-1/main.py
+++ Day20/snake-part-1/main.py
@@ -1,40 +1,28 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
 # Screen Window Setup
 screen = Screen()
-screen.setup(width = 600, height = 600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
-# Initialize variables
+snake = Snake()
 game_on = True
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
 
-for position in starting_positions:
-    new_segmenet = Turtle("square")
-    new_segmenet.color("yellow")
-    new_segmenet.penup()
-    new_segmenet.goto(position)
-    segments.append(new_segmenet)
-
+# Player controls
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 while game_on:
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
 
-
-
-
-
-
+    snake.move()
 
 screen.exitonclick()
