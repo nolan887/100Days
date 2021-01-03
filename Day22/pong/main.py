8cbbfa8c1e1dff9c39253bdf23cdba540c462052
nolan887 <nolan887@gmail.com>
2021-01-02 21:42:26 -0500

    Day 22 Start (Video 196)

---
+++ Day22/pong/main.py
@@ -0,0 +1,10 @@
+from turtle import Screen
+
+# Screen window setup
+screen = Screen()
+screen.setup(width=800,height=600)
+screen.bgcolor("black")
+screen.title("P O N G")
+
+
+screen.exitonclick()
