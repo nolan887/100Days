5c50cd8c27a975e4c441a8a753b6c5a6994a8541
nolan887 <nolan887@gmail.com>
2021-01-03 20:01:17 -0500

    Added functionality: ball increases speed every time it hits a paddle. Ball starts off pointed in the direction of the last person to score.

--- Day22/pong/ball.py
+++ Day22/pong/ball.py
@@ -2,28 +2,38 @@ from turtle import Turtle
 import random
 
 BALL_START_ANGLES = [15, 30, 45, 135, 150, 165, 195, 210, 225, 315, 330, 345]
-
+GO_RIGHT_ANGLES = [15, 30, 45, 315, 330, 345]
+GO_LEFT_ANGLES = [135, 150, 165, 195, 210, 225]
 
 class Ball(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.move_angle = BALL_START_ANGLES
         self.shape("circle")
         self.color("grey")
         self.penup()
-        self.new_point()
+        self.new_point("RANDOM")
         self.new_direction = 0
 
-    def new_point(self):
+
+    def new_point(self, dir):
         self.goto(0, 0)
-        self.new_direction = random.choice(BALL_START_ANGLES)
+        if dir == "left":
+            self.move_angle = GO_LEFT_ANGLES
+        elif dir == "right":
+            self.move_angle = GO_RIGHT_ANGLES
+        self.new_direction = random.choice(self.move_angle)
         self.setheading(self.new_direction)
+        self.ballspeed = 10
 
     def moving(self):
-        self.forward(10)
+        self.forward(self.ballspeed)
 
     def reverse(self):
         self.setheading((self.heading()) + 180)
+        self.ballspeed += 1
 
     def mirror(self):
         self.setheading((self.heading() * -1) + 180)
+        self.ballspeed += 1
