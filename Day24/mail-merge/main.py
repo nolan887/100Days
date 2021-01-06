347b36ab1df0fcfd72fcbd270d680a43ed6049e8
nolan887 <nolan887@gmail.com>
2021-01-06 18:13:33 -0500

    Day 24 progress - mail merge in work (Video 222)

--- Day24/mail-merge/main.py
+++ Day24/mail-merge/main.py
@@ -1,8 +1,26 @@
-# TODO: Create a letter using starting_letter.pages
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
+# Create a letter using starting_letter.pages
+    # for each name in invited_names.txt
+    # Replace the [name] placeholder with the actual name.
+    # Save the letters in the folder "ReadyToSend".
 
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+START_LINK = "./Input/Letters/starting_letter.txt"
+NAMES_LINK = "./Input/Names/invited_names.txt"
+OUTPUT_LINK = "./Output/ReadyToSend/"
+
+invited_names = []
+invite_save_files = []
+
+with open(NAMES_LINK) as f_invited:
+    invited = f_invited.readlines()
+
+for person in invited:
+    invited_names.append(person.strip())
+
+for name in invited_names:
+    invite_save_files.append(name + ".txt")
+
+# with open(START_LINK, "rt") as f_start:
+#     for name in invited_names:
+
