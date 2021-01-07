c553015417ae4b5ed8c835a6292489363f210eb7
nolan887 <nolan887@gmail.com>
2021-01-06 20:23:28 -0500

    Day 24 complete (Video 223)

--- Day24/mail-merge/main.py
+++ Day24/mail-merge/main.py
@@ -4,10 +4,10 @@
     # Save the letters in the folder "ReadyToSend".
 
 
-
 START_LINK = "./Input/Letters/starting_letter.txt"
 NAMES_LINK = "./Input/Names/invited_names.txt"
 OUTPUT_LINK = "./Output/ReadyToSend/"
+PLACEHOLDER = "[name]"
 
 invited_names = []
 invite_save_files = []
@@ -21,6 +21,10 @@ for person in invited:
 for name in invited_names:
     invite_save_files.append(name + ".txt")
 
-# with open(START_LINK, "rt") as f_start:
-#     for name in invited_names:
+with open(START_LINK, "rt") as f_start:
+    letter_contents = f_start.read()
+    for name in invited_names:
+        new_letter = letter_contents.replace(PLACEHOLDER, name)
+        with open((OUTPUT_LINK + name + "_invite.txt"), mode="w") as completed_letter:
+            completed_letter.write(new_letter)
 
