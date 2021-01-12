38b2d2e4607858ba6509dea2deda97fa4411674e
nolan887 <nolan887@gmail.com>
2021-01-11 20:19:37 -0500

    Day 27 intro to tkinter and positional arguments (Video 247)

--- Day27/day-27-start/main.py
+++ Day27/day-27-start/main.py
@@ -1,12 +1,20 @@
-import tkinter
+# import tkinter
+#
+# window = tkinter.Tk()
+# window.title("My first GUI Program")
+# window.minsize(width=500, height=500)
+#
+# my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+# my_label.pack(side="left")
+#
+#
+# window.mainloop()
 
-window = tkinter.Tk()
-window.title("My first GUI Program")
-window.minsize(width=500, height=500)
+def add(*args):
+    print(args[2])
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
 
-
-
-
-
-
-window.mainloop()
+print(add(3, 5, 6, 10, 15, 150, 345, 235, 3254))
