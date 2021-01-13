c4bb12b1af460129b370bac1ec9599845597c20a
nolan887 <nolan887@gmail.com>
2021-01-12 20:44:19 -0500

    Day 27 progress (Video 249)

--- Day27/day-27-start/main.py
+++ Day27/day-27-start/main.py
@@ -1,58 +1,37 @@
-# import tkinter
-#
-# window = tkinter.Tk()
-# window.title("My first GUI Program")
-# window.minsize(width=500, height=500)
-#
-# my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
-# my_label.pack(side="left")
-#
-#
-# window.mainloop()
+import tkinter
+
+window = tkinter.Tk()
+window.title("My first GUI Program")
+window.minsize(width=500, height=300)
 
 
-# def add(*args):
-#     print(args[2])
-#     sum = 0
-#     for n in args:
-#         sum += n
-#     return sum
-#
-# print(add(3, 5, 6, 10, 15, 150, 345, 235, 3254))
+# Making Labels
+my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.pack(side="left")
+
+# Both of the following two lines do the same thing
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+
+# Buttons
+
+def button_click():
+    print("I got clicked")
+    # my_label["text"] = "Button got Clicked"
+    # my_label.pack
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+button = tkinter.Button(text="Click Me", command=button_click)
+button.pack()
+
+
+# Entry
+input = tkinter.Entry(width=10)
+input.pack()
 
 
 
-# KWARGS
-def calculate(n, **kwargs):
-    print(kwargs)
-    for key, value in kwargs.items():
-        print(key)
-        print(value)
-    n += kwargs["add"]
-    n *= kwargs["multiply"]
-    print(n)
-
-calculate(2, add=3, multiply=5)
-
-class Car:
-
-    def __init__(self, **kwargs):
-        # self.make = kwargs["make"]
-        # self.model = kwargs["model"]
-        # This replaces the above code and protects code from crashing if there is not a kwarg assigned
-        self.make = kwargs.get("make")
-        self.model = kwargs.get("model")
-
-my_car = Car(make="Mazda", model="3 Hatch")
-print(my_car.make)
-print(my_car.model)
-
-ashleys_car = Car(model="Tiguan")
-print(ashleys_car.make)
-print(ashleys_car.model)
-
-def test(*args):
-    print(type(args))
-    print(args)
-
-test(1,2,3,4)
+window.mainloop()
---
+++ Day27/day-27-start/playground.py
@@ -0,0 +1,46 @@
+
+def add(*args):
+    print(args[2])
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
+
+print(add(3, 5, 6, 10, 15, 150, 345, 235, 3254))
+
+
+
+# KWARGS
+def calculate(n, **kwargs):
+    print(kwargs)
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add=3, multiply=5)
+
+class Car:
+
+    def __init__(self, **kwargs):
+        # self.make = kwargs["make"]
+        # self.model = kwargs["model"]
+        # This replaces the above code and protects code from crashing if there is not a kwarg assigned
+        self.make = kwargs.get("make")
+        self.model = kwargs.get("model")
+
+my_car = Car(make="Mazda", model="3 Hatch")
+print(my_car.make)
+print(my_car.model)
+
+ashleys_car = Car(model="Tiguan")
+print(ashleys_car.make)
+print(ashleys_car.model)
+
+def test(*args):
+    print(type(args))
+    print(args)
+
+test(1,2,3,4)
