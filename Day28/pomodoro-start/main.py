48c5876583bf5fd885515762f18657794dc7f100
nolan887 <nolan887@gmail.com>
2021-01-14 17:44:52 -0500

    GUI complete, ready for timer code (Video 255)

--- Day28/pomodoro-start/main.py
+++ Day28/pomodoro-start/main.py
@@ -17,17 +17,37 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# Setup Window
 window = Tk()
 window.title("Charles' Pomodoro Timer")
 window.config(padx=50, pady=50, bg=YELLOW)
 
+def start():
+    pass
+
+def reset():
+    pass
+
+# Setup Canvas
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(row=1,column=1)
 
+# Setup Labels
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME,50))
+timer_label.grid(row=0, column=1)
 
+check_label = Label(text="✓", bg=YELLOW, fg=GREEN, font=(FONT_NAME,24))
+check_label.grid(row=3, column=1)
+
+# Setup Buttons
+start_button = Button(text="Start", command=start, highlightthickness=0)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset", command=reset, highlightthickness=0)
+reset_button.grid(row=2, column=2)
 
 
 
