661c4dff5659260f8e7d95d0d7326df179465916
nolan887 <nolan887@gmail.com>
2021-01-21 21:14:21 -0500

    Day 33 progress - experimentation with JSON requests (Video 296)

--- Day33/day-33-start/main.py
+++ Day33/day-33-start/main.py
@@ -1,4 +1,17 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+response.raise_for_status()
+
+data = response.json()
+print(data)
+
+iss_position = response.json()["iss_position"]
+print(iss_position)
+
+iss_lat = response.json()["iss_position"]["latitude"]
+print(iss_lat)
+
+iss_long = response.json()["iss_position"]["longitude"]
+iss_loc = (iss_lat, iss_long)
+print(iss_loc)
