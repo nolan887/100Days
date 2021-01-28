bfdbc521e997ae53e6a611d64dd677205cd3cc99
nolan887 <nolan887@gmail.com>
2021-01-27 21:16:56 -0500

    Day 35 - Cleaned up formatting (Video 311)

--- Day35/rain-alert/main.py
+++ Day35/rain-alert/main.py
@@ -4,10 +4,15 @@ MY_LAT = 41.377289
 MY_LONG = -71.827461
 
 api_key = "c653eb49332fe60957ba3f8f37be99e5"
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 
+weather_params = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key
+}
 
-
-response = requests.get(url=f"https://api.openweathermap.org/data/2.5/onecall?lat={MY_LAT}&lon={MY_LONG}&appid={api_key}")
+response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 data = response.json()
 
