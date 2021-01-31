4cdb7c19284d6b5ce81d2b37ec06604e32aa8dbd
nolan887 <nolan887@gmail.com>
2021-01-31 09:53:57 -0500

    Day 38 - Workout tracking complete. (Video 333)

--- Day38/workout-tracker/main.py
+++ Day38/workout-tracker/main.py
@@ -2,7 +2,7 @@ import requests
 import os
 from datetime import datetime
 
-# --------------- SETUP - CONSTANTS --------------- #
+# --------------- API SETUP --------------- #
 nutrition_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 nutrition_key = os.environ.get("NUTRITIONIX_KEY")
 nutrition_app_id = os.environ.get("NUTRITIONIX_APP_ID")
@@ -13,6 +13,10 @@ headers = {
     "Content-Type": "application/json"
 }
 
+sheets_endpoint = "https://api.sheety.co/d3eeb7849c23a914bd8cf7c738fe696c/myWorkouts/workouts"
+sheet_token = os.environ.get("SHEETY_TOKEN")
+
+# --------------- DEFINING CONSTANTS --------------- #
 GENDER = "male"
 WEIGHT = 78.47
 HEIGHT = 182.88
@@ -27,6 +31,8 @@ else:
     age = age_year_end
 MY_AGE = age
 
+
+# --------------- ASKING FOR USER INPUT AND PASSING TO NUTRITIONIX TO REQUEST WORKOUT DATA --------------- #
 exercise_data = {
     "query": input("What excercise did you perform? (i.e: ran XX miles): "),
     "gender": GENDER,
@@ -35,5 +41,28 @@ exercise_data = {
     "age": MY_AGE
 }
 
-response = requests.post(url=nutrition_endpoint, json=exercise_data, headers=headers)
-print(response.text)
+workout_response = requests.post(url=nutrition_endpoint, json=exercise_data, headers=headers)
+workout_details = workout_response.json()["exercises"]
+
+# --------------- COMBINING WORKOUT DATA, DATE, AND TIME FOR PUSH TO SHEET --------------- #
+today_date = datetime.now().strftime("%m/%d/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in workout_details:
+    sheet_inputs = {
+        "workout":{
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+bearer_headers = {
+"Authorization": f"Bearer {sheet_token}"
+}
+
+
+sheet_response = requests.post(url=sheets_endpoint, json=sheet_inputs, headers=bearer_headers)
+print(sheet_response.text)
