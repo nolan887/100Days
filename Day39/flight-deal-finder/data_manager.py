8441440bf7d857aca8c1101f1f94070468bb6219
nolan887 <nolan887@gmail.com>
2021-02-01 20:11:12 -0500

    Day 39 - Capstone - Flight Tracker - Complete. Most of this was copy/pasted due to my unwillingness to provide bank account data to Tequila for the flight search API (Video 339)

--- Day39/flight-deal-finder/data_manager.py
+++ Day39/flight-deal-finder/data_manager.py
@@ -1,3 +1,32 @@
+import requests
+import os
+
+sheety_endpoint = "https://api.sheety.co/d3eeb7849c23a914bd8cf7c738fe696c/flightDeals/prices"
+sheety_token = os.environ.get("SHEETY_TOKEN")
+
+bearer_headers = {
+    "Authorization": f"Bearer {sheety_token}"
+}
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=sheety_endpoint, headers=bearer_headers)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{sheety_endpoint}/{city['id']}",
+                json=new_data,
+                headers=bearer_headers
+            )
--- Day39/flight-deal-finder/notification_manager.py
+++ Day39/flight-deal-finder/notification_manager.py
@@ -1,3 +1,21 @@
+from twilio.rest import Client
+import os
+
+TWILIO_SID = "AC96f51ba9845b52998cf4d9fdc593f577"
+TWILIO_AUTH_TOKEN = os.environ.get("TWILLIO_AUTH_TOKEN")
+TWILIO_VIRTUAL_NUMBER = "+16692013335"
+TWILIO_VERIFIED_NUMBER = os.environ.get("MY_NUMBER")
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
