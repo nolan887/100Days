a83009d18d1630278fe14af7759991ea15abd6e9
nolan887 <nolan887@gmail.com>
2021-02-10 21:06:03 -0500

    Day 50 - Tinder bot skipped. (Video 429)

---
+++ Day50/skipped/main.py
@@ -0,0 +1,8 @@
+# Skipped this project.
+
+# Goal is to use selenium web driver to auto-swipe tinder.
+
+# I feel confident enough with the past several projects that this might be slighly more challenging, but nothing earth
+#  shattering. I also do not feel comfortable setting up a tinder profile, even just for learning as it has hard links
+#  to my e-mail, google/facebook, and phone number.
+
