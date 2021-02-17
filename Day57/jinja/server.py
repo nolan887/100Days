3dfb3aa7d27e1f2a133f216bca29cb23432d8991
nolan887 <nolan887@gmail.com>
2021-02-16 20:03:42 -0500

    Day 57 Progress - For loops and if statements within .html files using Jinja (Video 467)

--- Day57/jinja/server.py
+++ Day57/jinja/server.py
@@ -38,5 +38,12 @@ def guess(name):
            f"<br>I'm {probability}% sure you are a {gender}" \
            f"<br>Age guess: {age}"
 
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
 if __name__ == "__main__":
     app.run(debug=True)
