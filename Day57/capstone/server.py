8ebf5f5bdaf5133b881f06e62c6d44a6a9091e59
nolan887 <nolan887@gmail.com>
2021-02-18 19:43:10 -0500

    Day 57 Complete - Capstone using jinja and code within html files and flask servers (Video 469)

--- Day57/capstone/server.py
+++ Day57/capstone/server.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts)
+
+@app.route('/post/<int:num>')
+def post(num):
+    blog_url = "https://api.npoint.io/5abcca6f4e39b4955965"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    blog_post = all_posts[num-1]
+    return render_template("post.html", post=blog_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
