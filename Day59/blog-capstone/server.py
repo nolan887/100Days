1edd8aff448f3d38cdd189cc9e226eedc51f5c00
nolan887 <nolan887@gmail.com>
2021-02-21 19:56:13 -0500

    Day 59 - Blog Capstone Progress - server setup (Video 502)

---
+++ Day59/blog-capstone/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+import random
+from datetime import datetime
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
