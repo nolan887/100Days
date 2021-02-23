09830bdb68ae9f28422992fe0b3b2b69b0d69046
nolan887 <nolan887@gmail.com>
2021-02-22 19:41:58 -0500

    Day 59 - Blog Capstone Complete - (Video 507)

--- Day59/blog-capstone/server.py
+++ Day59/blog-capstone/server.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template
-import random
-from datetime import datetime
 import requests
 
 app = Flask(__name__)
 
+posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
+
 @app.route('/')
+def index():
+    return render_template('index.html', posts=posts)
+
+@app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    requested_post = posts[index - 1]
+    return render_template("post.html", post=requested_post)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
