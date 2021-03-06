92cb018295dce536b0d46ea3c95e710265df905b
nolan887 <nolan887@gmail.com>
2021-03-05 20:28:34 -0500

    Day 61 - Bootcamp integration with Template Inheritance using Jinja (Video 522)

--- Day61/flask-secrets/server.py
+++ Day61/flask-secrets/server.py
@@ -20,7 +20,10 @@ def home():
 def login():
     form = MyForm()
     if form.validate_on_submit():
-        return render_template('success.html')
+        if form.email.data == "admin@email.com" and form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template("login.html", form=form)
 
 if __name__ == '__main__':
