8cbb38eaaec71bdfda0313f3ddb6a30cd4b91105
nolan887 <nolan887@gmail.com>
2021-03-22 21:03:08 -0400

    Day 68 - Progress - Added ability to create a new user and post to the database. Added welcome message for user based on the database 'name' field (Module 568)

--- Day68/flask-authorization/main.py
+++ Day68/flask-authorization/main.py
@@ -25,8 +25,18 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET","POST"])
 def register():
+    if request.method =="POST":
+        new_user = User(
+            email = request.form.get('email'),
+            name = request.form.get('name'),
+            password = request.form.get('password')
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        print(new_user.name)
+        return redirect(url_for("secrets", username=new_user.name))
     return render_template("register.html")
 
 
