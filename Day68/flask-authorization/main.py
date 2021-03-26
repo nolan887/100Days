3ab37016878016d3df107c930bdbea739e637251
nolan887 <nolan887@gmail.com>
2021-03-25 20:31:00 -0400

    Day 68 - Progress - Added flask flash messages to alert user to login or registration errors. (Module 575)

--- Day68/flask-authorization/main.py
+++ Day68/flask-authorization/main.py
@@ -36,6 +36,11 @@ def home():
 @app.route('/register', methods=["GET","POST"])
 def register():
     if request.method =="POST":
+
+        if User.query.filter_by(email=request.form.get('email')).first():
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
         user_pass = request.form.get('password')
         hashed_pw = werkzeug.security.generate_password_hash(user_pass, method='pbkdf2:sha256', salt_length=8)
         new_user = User(
@@ -55,8 +60,16 @@ def login():
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
+
         user = User.query.filter_by(email=email).first()
-        if check_password_hash(user.password, password):
+
+        if not user:
+            flash("This e-mail does not exist in the database, please try again.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash("Incorrect password, please try again.")
+            return redirect(url_for('login'))
+        else:
             login_user(user)
             return redirect(url_for('secrets'))
     return render_template("login.html")
